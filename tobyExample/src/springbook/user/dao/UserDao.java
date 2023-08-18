052774b9f58225ab14f25847f1ab80dc2cad7867
yelo-o <flyordig@gmail.com>
2023-08-18 09:21:54 +0900

    중복코드(DB연결) 추출 및 분리

--- tobyExample/src/springbook/user/dao/UserDao.java
+++ tobyExample/src/springbook/user/dao/UserDao.java
@@ -6,9 +6,7 @@ import java.sql.*;
 
 public class UserDao {
     public void add(User user) throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection c = DriverManager.getConnection(
-                "jdbc:mysql://localhost/springbook", "root", "kosta");
+        Connection c = getConnection();
         PreparedStatement ps = c.prepareStatement(
                 "insert into users(id, name, password) values(?,?,?)");
         ps.setString(1, user.getId());
@@ -21,9 +19,7 @@ public class UserDao {
         c.close();
     }
     public User get(String id) throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection c = DriverManager.getConnection(
-                "jdbc:mysql://localhost/springbook", "root", "kosta");
+        Connection c = getConnection();
         PreparedStatement ps = c.prepareStatement(
                 "select * from users where id=?");
         ps.setString(1, id);
@@ -40,5 +36,12 @@ public class UserDao {
         c.close();
 
         return user;
+    }
+
+    private Connection getConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection c = DriverManager.getConnection(
+                "jdbc:mysql://localhost/springbook", "root", "kosta");
+        return c;
     }
 }
